77ddf2173386441e351352e43adfd8e667d07ca5
David Boyce <dboyce@broadcom.com>
2016-08-22 18:38:03 -0700

    For now, build my old custom libgit2 branch. Update someday.

--- OPS/libgit2/build-for-ao.sh
+++ OPS/libgit2/build-for-ao.sh
@@ -3,19 +3,23 @@
 set -e
 if type -t git >/dev/null; then
     if [[ -d repo/.git ]]; then
-	echo "Skip automatic update, in order to keep multiple platforms at same rev"
+	echo "Skip automatic update in order to keep multiple platforms at same rev"
 	# (cd repo && git pull)
     else
-	git clone git://github.com/libgit2/libgit2.git repo
+	# (set -x; git clone git://github.com/libgit2/libgit2.git repo)
+	# For now, build my old custom branch. Update someday.
+	(set -x; git clone git@github.com:boyski/libgit2.git repo)
     fi
 fi
 : ${OS_CPU:=`uname -m`_`uname -s`}
 rm -rf ${OS_CPU?}
-mkdir ${OS_CPU?}
-cd ${OS_CPU?}
+mkdir $OS_CPU
+set -x
+cd $OS_CPU
 cmake -DBUILD_SHARED_LIBS=NO \
     -DCMAKE_OSX_ARCHITECTURES="i386;x86_64" \
     -DCMAKE_BUILD_TYPE=Release \
+    --build \
     "$@" \
     ../repo
 make VERBOSE=1
